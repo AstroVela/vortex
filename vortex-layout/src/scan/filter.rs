ae36aa8cdf3cc5376322f146432da267b51b7c16
Matt Katz <mhkatz97@gmail.com>
2026-08-11 19:15:53 -0700

    Fix lambda expression integration
    Signed-off-by: Matt Katz <mhkatz97@gmail.com>

--- vortex-layout/src/scan/filter.rs
+++ vortex-layout/src/scan/filter.rs
@@ -186,12 +186,12 @@ mod tests {
         assert_eq!(
             conjuncts
                 .iter()
-                .map(|expr| expr.dtype().cloned())
+                .map(|expr| expr.dtype().clone())
                 .collect::<Vec<_>>(),
             vec![
-                Some(DType::Bool(Nullability::Nullable)),
-                Some(DType::Bool(Nullability::Nullable)),
-                Some(DType::Bool(Nullability::NonNullable)),
+                DType::Bool(Nullability::Nullable),
+                DType::Bool(Nullability::Nullable),
+                DType::Bool(Nullability::NonNullable),
             ]
         );
         Ok(())
