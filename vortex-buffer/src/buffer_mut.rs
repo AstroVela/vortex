473d53744579c0f9e492a6f42640b785f1d418c2
Nicholas Gates <nick@nickgates.com>
2026-08-28 15:06:35 -0400

    Revert "fix(buffer): account for alignment when doubling"
    This reverts commit b55382807f5bf40accd7d456cb6ff25b0443b233.
    
    Signed-off-by: Nicholas Gates <nick@nickgates.com>

--- vortex-buffer/src/buffer_mut.rs
+++ vortex-buffer/src/buffer_mut.rs
@@ -450,16 +450,11 @@ impl<T> BufferMut<T> {
             .checked_mul(size_of::<T>())
             .vortex_expect("buffer capacity overflow");
         let physical_alignment = max(self.alignment, self.allocation.buffer_alignment());
-        let padding = *physical_alignment - 1;
         let required_size = required_size
-            .checked_add(padding)
+            .checked_add(*physical_alignment)
             .vortex_expect("buffer capacity overflow");
         let current_size = self.allocation.size() - self.offset;
-        let doubled_size = current_size
-            .checked_mul(2)
-            .and_then(|size| size.checked_add(padding))
-            .unwrap_or(usize::MAX);
-        let allocation_size = required_size.max(doubled_size);
+        let allocation_size = required_size.max(current_size.saturating_mul(2));
         let layout = Layout::from_size_align(allocation_size, 1)
             .unwrap_or_else(|_| vortex_panic!("buffer capacity exceeds maximum allocation size"));
 
